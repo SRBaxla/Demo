810898fdc6fab5d1053b851d6fe411f97b9bd7ba
Sudeep Richard Baxla <srb7309264849@gmail.com>
2022-11-19 18:53:21 +0530

    nothing

--- Program.cs
+++ Program.cs
@@ -9,19 +9,42 @@ using CsvHelper.Configuration.Attributes;
 namespace Csvreader
 {
     class Program
-    {
+    {   
+        public static sameyear(int year)
+        {
+
+        }
         static void Main(string[] args)
         {
             using( var streamReader = new StreamReader(@"C:\Users\srb73\Desktop\Demo\csvfile\Input_v1.0.csv"))
             {
                 using( var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
                 {
-                    var records=csvReader.GetRecords<andaze>().ToList();
+                    var records=csvReader.GetRecords<Andaze>().ToList();
+
+                    //List<Person> persons = students.Select(student => new Person {FirstName = student.FirstName, LastName = student.LastName}).ToList();
+                    List<OUTPUT> output = records.Select(records => new OUTPUT {Date=records.DATE, Grain=records.grain,BeerProduction=records.BeerProduction, Manager=records.FactoryManagerName}).ToList();
+                    List<compute> computes = output.Select(output => new compute {year=output.Date.Year,BeerProduction=output.BeerProduction}).ToList();
+                    foreach (compute i in computes)
+                    {
+                        var year=i.year;
+                        if(year == i.year)
+                        {
+                            computes.BinaryS
+                        }
+                    }
                 }
             }
 
         }
-        public class andaze
+        public class compute
+        {
+            public int year{get; set;}
+            public double BeerProduction{get;set;}
+            public double YearMean{get;set;}
+            public double YearMedian{get;set;}
+        }
+        public class Andaze
         {
             //DATE,grain,BeerProduction,FactoryManagerName
             [Name("DATE")]
@@ -29,9 +52,23 @@ namespace Csvreader
             [Name("grain")]
             public string grain{get; set;}
             [Name("BeerProduction")]
-            public int BeerProduction{get; set;}
+            public double BeerProduction{get; set;}
             [Name("FactoryManagerName")]
             public string FactoryManagerName{get; set;}
+            
+        }
+        public class OUTPUT
+        {
+            public DateOnly Date{get; set;}
+            public string Grain{get; set;}
+            public double BeerProduction{get; set;}
+            public string Manager{get; set;}
+
+            public double YearMean{get; set;}
+
+            public double YearMedian{get; set;}
+
+            public string IsBearProduct{get; set;}
         }
     }
 }
